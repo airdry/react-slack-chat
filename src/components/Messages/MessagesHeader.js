d37b261599a57b310f9c45f06834357f5bfff6a4
airdry <flyside@bk.ru>
2020-01-01 15:51:03 +0300

    feat: add search messages

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,6 +3,13 @@ import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 
 class MessagesHeader extends React.Component {
   render() {
+    const {
+      channelName,
+      numUniqueUsers,
+      handleSearchChange,
+      searchLoading
+    } = this.props;
+
     return (
       <Segment clearing>
         {/* Channel Title */}
@@ -13,15 +20,17 @@ class MessagesHeader extends React.Component {
           style={{ marginBottom: 0 }}
         >
           <span>
-            Channel
+            {channelName}
             <Icon name={'star outline'} color="black" />
           </span>
-          <Header.Subheader>2 Users</Header.Subheader>
+          <Header.Subheader>{numUniqueUsers}</Header.Subheader>
         </Header>
 
         {/* Channel Search Input */}
         <Header floated="right">
           <Input
+            loading={searchLoading}
+            onChange={handleSearchChange}
             size="mini"
             icon="search"
             name="searchTerm"
